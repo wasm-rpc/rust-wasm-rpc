05ed5ab0446187924c8eb040ae4c749dc343f4e0
Mason Fischer <mason@kissr.co>
2018-09-05 12:17:21 -0400

    Restructure wasm-rpc into wasm-rpc  and wasm-rpc-macros

--- wasm-rpc/src/lib.rs
+++ wasm-rpc/src/lib.rs
@@ -1,92 +1,33 @@
 #![no_std]
 #![feature(
+    try_trait,
     alloc,
     core_intrinsics,
+    alloc_error_handler,
+    panic_implementation,
     )]
 #[macro_use] extern crate alloc;
 extern crate cbor_no_std;
+#[cfg(test)]
+extern crate ellipticoin_test_framework as ellipticoin;
 mod error;
 pub mod memory;
 pub use error::{Error};
-use alloc::String;
-use alloc::Vec;
+use alloc::string::String;
+use alloc::vec::Vec;
 use cbor_no_std::{from_bytes, to_bytes, Value};
 use core::mem;
 use core::slice;
+use core::intrinsics::transmute;
 
 pub const LENGTH_BYTE_COUNT: usize = 4;
 
 pub type Pointer = *const u8;
 
-fn u32_to_u8_vec(x:u32, big_endian: bool) -> Vec<u8> {
-    let b1 : u8 = ((x >> 24) & 0xff) as u8;
-    let b2 : u8 = ((x >> 16) & 0xff) as u8;
-    let b3 : u8 = ((x >> 8) & 0xff) as u8;
-    let b4 : u8 = (x & 0xff) as u8;
-
-    if big_endian {
-        vec![b1, b2, b3, b4]
-    } else {
-        vec![b4, b3, b2, b1]
-    }
-}
-
-fn u64_to_u8_vec(x: u64, big_endian: bool) -> Vec<u8> {
-    let b1 : u8 = ((x >> 56) & 0xff) as u8;
-    let b2 : u8 = ((x >> 48) & 0xff) as u8;
-    let b3 : u8 = ((x >> 40) & 0xff) as u8;
-    let b4 : u8 = ((x >> 32) & 0xff) as u8;
-    let b5 : u8 = ((x >> 24) & 0xff) as u8;
-    let b6 : u8 = ((x >> 16) & 0xff) as u8;
-    let b7 : u8 = ((x >> 8) & 0xff) as u8;
-    let b8 : u8 = (x & 0xff) as u8;
-
-    if big_endian {
-        vec![b1, b2, b3, b4, b5, b6, b7, b8]
-    } else {
-        vec![b8, b7, b6, b5, b4, b3, b2, b1]
-    }
-}
-
-fn u8_vec_to_u32(x: Vec<u8>, big_endian: bool) -> u32 {
-    if big_endian {
-        (x[0] as u32) << 24 |
-            ((x[1] as u32) & 0xff) << 16 |
-            ((x[2] as u32) & 0xff) << 8 |
-            ((x[3] as u32) & 0xff)
-    } else {
-        (x[3] as u32) << 24 |
-            ((x[2] as u32) & 0xff) << 16 |
-            ((x[1] as u32) & 0xff) << 8 |
-            ((x[0] as u32) & 0xff)
-    }
-}
-
-fn u8_vec_to_u64(x: Vec<u8>, big_endian: bool) -> u64 {
-    if big_endian {
-        (x[7] as u64) << 56 |
-            ((x[6] as u64) & 0xff) << 48 |
-            ((x[5] as u64) & 0xff) << 40 |
-            ((x[4] as u64) & 0xff) << 32 |
-            ((x[3] as u64) & 0xff) << 24 |
-            ((x[2] as u64) & 0xff) << 16 |
-            ((x[1] as u64) & 0xff) << 8 |
-            ((x[0] as u64) & 0xff)
-    } else {
-        (x[0] as u64) << 56 |
-            ((x[1] as u64) & 0xff) << 48 |
-            ((x[2] as u64) & 0xff) << 40 |
-            ((x[3] as u64) & 0xff) << 32 |
-            ((x[4] as u64) & 0xff) << 24 |
-            ((x[5] as u64) & 0xff) << 16 |
-            ((x[6] as u64) & 0xff) << 8 |
-            ((x[7] as u64) & 0xff)
-    }
-}
-
 pub unsafe trait Dereferenceable {
     fn as_raw_bytes(&self) -> Vec<u8>;
     fn to_bytes(&self) -> Vec<u8>;
+    fn to_int(&self) -> u64;
     fn to_string(&self) -> String;
     fn to_array(&self) -> Vec<Value>;
 }
@@ -94,7 +35,9 @@ pub unsafe trait Dereferenceable {
 unsafe impl Dereferenceable for Pointer {
     fn as_raw_bytes(&self) -> Vec<u8> {
         let length_slice = unsafe { slice::from_raw_parts(self.offset(0) as *const u8, LENGTH_BYTE_COUNT as usize) };
-        let length = u8_vec_to_u32(length_slice.to_vec(), true);
+        let mut length_slice_four: [u8; 4] = [0; 4];
+        length_slice_four.copy_from_slice(&length_slice[..]);
+        let length = unsafe{transmute::<[u8; 4], u32>(length_slice_four)};
 
         unsafe {
             slice::from_raw_parts(self.offset(LENGTH_BYTE_COUNT as isize) as *const u8, length as usize).to_vec()
@@ -103,6 +46,10 @@ unsafe impl Dereferenceable for Pointer {
 
     fn to_bytes(&self) -> Vec<u8> {
         from_bytes(self.as_raw_bytes()).as_bytes().unwrap().to_vec()
+    }
+
+    fn to_int(&self) -> u64 {
+        from_bytes(self.as_raw_bytes()).as_int().unwrap()
     }
 
     fn to_string(&self) -> String {
@@ -123,7 +70,7 @@ pub unsafe trait Referenceable {
 unsafe impl Referenceable for Vec<u8> {
     fn as_pointer(&self) -> Pointer {
         let mut value = self.clone();
-        let mut value_and_length = u32_to_u8_vec(value.len() as u32, true);
+        let mut value_and_length = unsafe{transmute::<u32, [u8; 4]>(value.len() as u32)}.to_vec();
         value_and_length.append(&mut value);
         let value_and_length_ptr = value_and_length.as_ptr();
         mem::forget(value_and_length);
@@ -155,7 +102,7 @@ impl Responsable for Result<Value, Error> {
             Err(error) => to_bytes(Value::String(error.message.into())),
         };
 
-        let mut error_code_bytes = u32_to_u8_vec(error_code, true);
+        let mut error_code_bytes = unsafe{transmute::<u32, [u8; 4]>(error_code as u32)}.to_vec();
 
         error_code_bytes.append(&mut return_value);
         error_code_bytes.as_pointer()
@@ -180,7 +127,7 @@ impl Responsable for Result<u32, Error> {
     }
 }
 
-impl Responsable for Result<Vec<u8>, Error> {
+impl Responsable for Result<u64, Error> {
     fn to_response(self) -> Pointer {
         match self {
             Ok(value) => (Ok::<Value, Error>(value.into())).to_response(),
@@ -189,11 +136,12 @@ impl Responsable for Result<Vec<u8>, Error> {
     }
 }
 
-pub trait FromBytes {}
-
-impl FromBytes {
-    pub fn from_u64(value: u64) -> Vec<u8>{
-        u64_to_u8_vec(value, true)
+impl Responsable for Result<Vec<u8>, Error> {
+    fn to_response(self) -> Pointer {
+        match self {
+            Ok(value) => (Ok::<Value, Error>(value.into())).to_response(),
+            Err(error) => (Err::<Value, Error>(error)).to_response()
+        }
     }
 }
 
@@ -206,7 +154,7 @@ impl Bytes<u64> for Vec<u8> {
         if self.len() == 8 {
             let mut slice: [u8; 8] = [0; 8];
             slice.copy_from_slice(&self[..]);
-            u8_vec_to_u64(slice.to_vec(), false)
+            unsafe{transmute::<[u8; 8], u64>(slice)}
         } else {
             0
         }
@@ -215,8 +163,21 @@ impl Bytes<u64> for Vec<u8> {
 
 impl Bytes<u32> for Vec<u8> {
     fn value(&self) -> u32 {
-        let mut slice: [u8; 4] = [0; 4];
-        slice.copy_from_slice(&self[..]);
-        u8_vec_to_u32(slice.to_vec(), true)
+        if self.len() == 4 {
+            let mut slice: [u8; 4] = [0; 4];
+            slice.copy_from_slice(&self[..]);
+            unsafe{transmute::<[u8; 4], u32>(slice)}
+        } else {
+            0
+        }
+    }
+}
+
+impl From<core::option::NoneError> for Error {
+    fn from(mut value: core::option::NoneError) -> Error {
+        Error {
+            code: 1,
+            message: "None Error",
+        }
     }
 }
